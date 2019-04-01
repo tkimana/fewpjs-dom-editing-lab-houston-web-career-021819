8de5409d5f9f8947121c1139aab9f43763c976b5
Theodore Kimana <kimana.theodore@gmail.com>
2019-04-01 18:40:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><h1>My HTML adventure</h1></title>
+  <p>
+  We're writing<a href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</a> markup to display in our <strong>browser</strong>. HTML markup to display in our <strong>browser</strong>.
+  We're basically telling computers what to do. <em>Neat!</em>
+</p>
+  
+</head>
+<body>
+  <!--All our work for this lesson will go here-->
+</body>
+</html>
